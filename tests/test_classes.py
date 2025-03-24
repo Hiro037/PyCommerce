d9d83ae1b0aedbe577b7715425f6759c110756a6
Said Magomedov <saidmagomedov037@gmail.com>
2025-03-24 18:27:02 +0500

    README.md added

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,5 +1,6 @@
-from src.classes import Product,Category
+from src.classes import Product, Category
 import pytest
+
 
 @pytest.fixture()
 def create_product():
@@ -12,10 +13,12 @@ def create_category():
     Category.category_count = 0
     Category.product_count = 0
 
+
 def test_category_init(create_category):  # Тест на корректность инициализации Category
     assert create_category.name == 'Fruits'
     assert create_category.description == 'Lots of goodies'
     assert create_category.products == ['Apple', 'Banana']
+
 
 def test_product_init(create_product):  # Тест на корректность инициализации Product
     assert create_product.name == 'Apple'
@@ -23,8 +26,10 @@ def test_product_init(create_product):  # Тест на корректность
     assert create_product.price == 10.00
     assert create_product.quantity == 5
 
+
 def test_product_count(create_category):  # Тест на подсчет количества продуктов
     assert Category.product_count == 2
+
 
 def test_category_count(create_category):  # Тест на подсчет количества категорий
     assert Category.category_count == 1
